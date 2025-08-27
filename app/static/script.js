652fede08a46f3ac5bf14067e9d9d47e7228e019
Mayur Mahajan <mayuryesh@gmail.com>
2025-08-27 23:12:50 +0530

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,4 +1,4 @@
 function updateStatus() {
     const statusElem = document.getElementById("status");
-    statusElem.textContent = "All systems operational-Devops DEVVVVVVV !!! ✅";
+    statusElem.textContent = "All systems operational-Development Environment !!! ✅";
 }
